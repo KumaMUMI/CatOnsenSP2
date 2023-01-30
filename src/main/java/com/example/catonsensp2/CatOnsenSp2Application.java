1bbd64909602cf2dca4a0193c269e7661abfcb0e
KumaMUMI <olozatarebunk@gmail.com>
2023-01-30 21:53:29 +0700

    first commit

---
+++ src/main/java/com/example/catonsensp2/CatOnsenSp2Application.java
@@ -0,0 +1,13 @@
+package com.example.catonsensp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatOnsenSp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatOnsenSp2Application.class, args);
+    }
+
+}
